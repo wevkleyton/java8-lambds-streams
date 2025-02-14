8da1b37b5b6259202273673fde1bb2b3bb65cbf9
Wev kleyton <kleyton.cpd@gmnail.com>
2025-02-14 17:37:21 -0300

    Inicio projeto estudo java com lambdas e streams

---
+++ src/main/java/br/com/java/pratico/PraticoApplication.java
@@ -0,0 +1,13 @@
+package br.com.java.pratico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraticoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraticoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/java/pratico/PraticoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.java.pratico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraticoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
